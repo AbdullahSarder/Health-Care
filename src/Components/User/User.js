fe04c599f2a04177d860aeb95061568a0c011bab
AbdullahSarder <57371591+AbdullahSarder@users.noreply.github.com>
2021-10-20 23:35:33 +0600

    Write Anything

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Container, Image, Row } from 'react-bootstrap';
+import useAuth from '../../hooks/useAuth';
+import './user.css'
 
 const User = () => {
+    const{user}=useAuth();
     return (
         <div>
-            <h1>User</h1>
+            
+            <Container fluid style={{ 
+                            backgroundImage: "url(/Images/about/about-3.jpg)",
+                            paddingTop:100,
+                            paddingBottom:100,
+                            backgroundSize:'cover'
+                            }}>
+                        <Row className="">
+                        <div className="col-md-12">
+                            <div className="block text-center">
+                            <Image className="profile" src={user.photoURL}></Image>
+                            <h1 className="text-capitalize mb-5 text-lg">{user.displayName}</h1>
+                        </div>
+                        </div>
+                        </Row>
+            </Container>
         </div>
     );
 };
